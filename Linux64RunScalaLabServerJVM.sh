72e9f899672ef51d3e73bedb5d0ad2ddca600cc7
sterglee <sterg@teikav.edu.gr>
2018-01-23 12:26:00 +0200

    using ScalaLab features from Scala REPL

--- Linux64RunScalaLabServerJVM.sh
+++ Linux64RunScalaLabServerJVM.sh
@@ -1,1 +1,1 @@
-java   -server -d64      -XX:+UseNUMA -XX:+UseParallelGC -XX:+UseCompressedOops -XX:+AggressiveOpts -Djava.library.path=.:./lib:./libCUDA -Xss5m -Xms4000m -Xmx23500m -jar ScalaLab.jar   &
+java   -server -d64      -XX:+UseNUMA -XX:+UseParallelGC -XX:+UseCompressedOops -XX:+DoEscapeAnalysis  -XX:+AggressiveOpts -Djava.library.path=.:./lib:./libCUDA -Xss5m -Xms4000m -Xmx23500m -jar ScalaLab.jar   &
--- MacOSXRunScalaLabServerJVM.sh
+++ MacOSXRunScalaLabServerJVM.sh
@@ -1,1 +1,1 @@
-java  -XX:+UseNUMA -XX:+UseParallelGC -XX:+UseCompressedOops -XX:+AggressiveOpts -Djava.library.path=.:./lib:. -Xss5m -Xms2000m -Xmx23500m -jar ScalaLab.jar &
+java  -XX:+UseNUMA -XX:+UseParallelGC -XX:+UseCompressedOops -XX:+AggressiveOpts -XX:+DoEscapeAnalysis  -Djava.library.path=.:./lib:. -Xss5m -Xms2000m -Xmx23500m -jar ScalaLab.jar &
